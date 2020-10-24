5554a86cb66c4c1877a325996795fdb4b8926230
MikeWhalenIII <michael1327@hotmail.com>
2020-10-24 13:08:12 -0400

    Added Product model and interface

---
+++ Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutdoorGear_Store.Models
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
