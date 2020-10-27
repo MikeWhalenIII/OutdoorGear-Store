db999d928b3fec866225d5ffe1e75dc2a843239c
MikeWhalenIII <michael1327@hotmail.com>
2020-10-26 22:17:37 -0400

    SQL packages

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,7 @@ namespace OutdoorGear_Store
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=List}/{id?}");
             });
         }
     }
