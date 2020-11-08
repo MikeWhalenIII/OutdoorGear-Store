65bc1b0c4a04687d68a46d209082b3729466de38
MikeWhalenIII <michael1327@hotmail.com>
2020-11-08 16:22:47 -0500

    Added pagination

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OutdoorGear_Store.Models;
+using OutdoorGear_Store.Models.ViewModels;
 
 namespace OutdoorGear_Store.Controllers
 {
@@ -11,10 +12,23 @@ namespace OutdoorGear_Store.Controllers
     {
         private IProductRepository repository;
         public int PageSize = 4;
-        public ProductController(IProductRepository repo) => repository = repo;
-        public ViewResult List(int page = 1) => View(repository.Products
-                                    .OrderBy(p => p.ProductID)
-                                    .Skip((page - 1) * PageSize)
-                                    .Take(PageSize));
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List(int page = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
