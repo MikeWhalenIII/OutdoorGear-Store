e18d857d017a02e9444a807a92d03c17cf192119
MikeWhalenIII <michael1327@hotmail.com>
2020-12-03 07:58:10 -0500

    Added AdminController

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OutdoorGear_Store.Models;
+
+namespace OutdoorGear_Store.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Products);
+    }
+}
